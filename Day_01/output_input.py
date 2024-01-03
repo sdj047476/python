a1fbc6b412dcaff9b59fc60500871ce2b611137b
sdj047476 <155497581+sdj047476@users.noreply.github.com>
2024-01-03 12:51:32 +0900

    Add files via upload

---
+++ Day_01/output_input.py
@@ -0,0 +1,26 @@
+# 주석문(코드 실행 안됨)
+print("이성욱 : AI소프트웨어융합학부 합격")
+
+# f문자열문법 : print(f" ") 연산이 들어가는 경우 {} 사용하기
+print(f"123+456+789={123+456+789}")
+
+# 변수 : 값을 담는 상자
+
+# name변수가 사용됨
+# '=' 넣는다 (같다와는 느낌이 다르다)
+name = "이성욱"
+mbti = "infp"
+print(name)
+print(mbti)
+print(f"저의 이름은 {name} mbti는 {mbti}")
+
+# input() [유저에게 입력값을 받는 기능]
+your_name = input("당신의 이름은:")
+print (f"당신의 이름은 {your_name}이군요!")
+
+
+
+
+
+
+
