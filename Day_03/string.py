d7f9b6df4e9722486bebc4f49a2c2d294becf8ce
sdj047476 <155497581+sdj047476@users.noreply.github.com>
2024-01-05 12:49:49 +0900

    Add files via upload

---
+++ Day_03/string.py
@@ -0,0 +1,15 @@
+
+
+# 데이터 타입에 따라서 뒤에 나오는 기능들이 다르게 나온다.
+
+coffee = "megacoffee"
+print(coffee.upper()) #MEGACOFFEE
+
+computer = ['C언어', 'JAVA', 'Python']
+computer.append('엑셀')
+print(computer)
+
+
+
+
+
